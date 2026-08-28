4811e9d795b2272d8c75870e79e0a50b53bcc1e2
Prasoon Rai <prasoonrai181@gmail.com>
2026-08-28 15:23:41 +0530

    Updated code to display all video related properties mentioned in phase 1 of building the project.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
     println!("Welcome to open media player");
+    ffmpeg::init().unwrap();
+
+    let file_path:String = String::new();
+
+    let context = ffmpeg::format::input(&file_path).unwrap();
+    let duration = context.duration();
+
+    for stream in context.streams() {
+        let codec_parameters = stream.parameters();
+        match codec_parameters.medium() {
+            ffmpeg::media::Type::Video => {
+                let context_decoder = ffmpeg::codec::context::Context::from_parameters(codec_parameters).unwrap();
+                let decoder = context_decoder.decoder().video().unwrap();
+
+                let video_resolution = (decoder.width(), decoder.height());
+                let frame_rate = stream.avg_frame_rate();
+
+                println!("Resulution of video: {} x {}", video_resolution.0, video_resolution.1);
+                println!("Video FPS: {}", frame_rate);
+            }
+            _ => {}
+        }
+    }
+    println!("Duration of video: {}", duration);
 }
