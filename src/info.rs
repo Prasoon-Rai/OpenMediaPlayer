c031e964d87eec67dee44f6bc30ac5486126e216
Prasoon Rai <prasoonrai181@gmail.com>
2026-08-31 13:07:41 +0530

    The window now displays the info of the mp4 file.

--- src/info.rs
+++ src/info.rs
@@ -1,17 +1,17 @@
 use ffmpeg::Rational;
 
 pub struct VideoInfo {
-    resolution: (u32, u32),
-    framerate: Rational,
-    durationSec: f64,
-    durationMin: f64,
+    pub resolution: (u32, u32),
+    pub framerate: Rational,
+    pub durationSec: f32,
+    pub durationMin: f32,
 }
 
 pub fn get_info(file_path: &str) -> Option<VideoInfo> {
     ffmpeg::init().unwrap();
 
     let context = ffmpeg::format::input(&file_path).ok()?;
-    let duration = context.duration() as f64 / ffmpeg::ffi::AV_TIME_BASE as f64;
+    let duration = context.duration() as f32 / ffmpeg::ffi::AV_TIME_BASE as f32;
 
     let mut vid_resolution = (0, 0);
     let mut frame_rate = ffmpeg::Rational::new(0, 1);
@@ -21,7 +21,7 @@ pub fn get_info(file_path: &str) -> Option<VideoInfo> {
         match codec_parameters.medium() {
             ffmpeg::media::Type::Video => {
                 let context_decoder =
-                ffmpeg::codec::context::Context::from_parameters(codec_parameters).ok()?;
+                    ffmpeg::codec::context::Context::from_parameters(codec_parameters).ok()?;
                 let decoder = context_decoder.decoder().video().ok()?;
 
                 vid_resolution = (decoder.width(), decoder.height());
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod info;
 use eframe::egui;
-use egui::{RichText, FontId, Color32};
+use egui::{RichText};
+use crate::info::VideoInfo;
 
 #[derive(Default)]
 struct MyApp {
-
+    info: Option<VideoInfo>
 }
 
 impl eframe::App for MyApp {
@@ -13,15 +14,28 @@ impl eframe::App for MyApp {
             ui.set_theme(egui::ThemePreference::System);
             ui.label(RichText::new("Welcome to open media player!").size(25.0));
             ui.separator();
-            ui.heading("MP4 info:")
+            ui.heading("MP4 info:");
+
+            match &self.info {
+                Some(v) => {
+                    ui.label(format!("Resolution: {} x {}", v.resolution.0, v.resolution.1));
+                    ui.label(format!("Framerate: {:.2} fps", f64::from(v.framerate)));
+                    ui.label(format!("Duration: {} sec ({} min)", v.durationSec, v.durationMin));
+                }
+                _ => {
+                    ui.label("Couldn't fetch mp4 info!");
+                }
+            }
         });
     }
 }
 
 fn main() -> eframe::Result {
+    let info = info::get_info("test.mp4");
+
     eframe::run_native(
         "Open Media Player",
         eframe::NativeOptions::default(),
-        Box::new(|_cc| Ok(Box::new(MyApp::default()))),
+        Box::new(|_cc| Ok(Box::new(MyApp { info }))),
     )
 }
